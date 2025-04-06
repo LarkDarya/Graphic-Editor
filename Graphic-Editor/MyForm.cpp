dbeb53063d0aa0b378584954c9f1e31a50393905
Lisa. <darya.zhavoronkova25052005@gmail.com>
2025-04-06 18:03:16 +0300

    Первый коммит

--- Graphic-Editor/MyForm.cpp
+++ Graphic-Editor/MyForm.cpp
@@ -1,2 +1,13 @@
 #include "MyForm.h"
 
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThread]
+void main(array<String^>^ arg) {
+    Application::EnableVisualStyles();
+    Application::SetCompatibleTextRenderingDefault(false);
+
+    mathpro::hello form; // mathpro название проекта , hello название фрмы (вашей)
+    Application::Run(% form);
+}
