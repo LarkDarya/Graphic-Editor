40805a5e045bc4ed93ea28c73eacbbedc11b00d1
LarkDarya <darya.zhavoronkova25052005@gmail.com>
2025-05-08 19:32:53 +0300

    Добавлен класс тригонометрических функций

--- Function.h
+++ Function.h
@@ -37,5 +37,53 @@ public:
     QString getName() const override { return "Polynomial"; }
 };
 
+// Тригонометрические функции
+class TrigonometricFunction : public Function {
+public:
+    enum Type { Sin, Cos, Tan, Cot };
+private:
+    Type funcType;
+    QVector<double> coefficients;
+
+public:
+    explicit TrigonometricFunction(Type type) : funcType(type), coefficients({1.0, 0.0}) {}
+
+    double evaluate(double x) const override {
+        double amplitude = coefficients.size() > 0 ? coefficients[0] : 1.0;
+        double shift = coefficients.size() > 1 ? coefficients[1] : 0.0;
+        double val = x + shift;
+
+        switch(funcType) {
+        case Sin: return amplitude * sin(val);
+        case Cos: return amplitude * cos(val);
+        case Tan: return amplitude * tan(val);
+        case Cot: {
+            double t = tan(val);
+            return (t != 0) ? amplitude / t : 0;
+        }
+        }
+        return 0;
+    }
+
+    void setCoefficients(const QVector<double>& coeffs) override {
+        coefficients = coeffs;
+    }
+
+    QVector<double> getCoefficients() const override {
+        return coefficients;
+    }
+
+    QString getName() const override {
+        switch(funcType) {
+        case Sin: return "Sin";
+        case Cos: return "Cos";
+        case Tan: return "Tan";
+        case Cot: return "Cot";
+        }
+        return "Trigonometric";
+    }
+};
+
+
 
 #endif // FUNCTION_H
